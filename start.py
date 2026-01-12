d3d027e3d7ffd5fd061d3ee5ba8e3a4b7d579986
Mobassar@4u <92091277+Mobassar4u@users.noreply.github.com>
2026-01-12 22:52:04 +0530

    Update start.py

--- start.py
+++ start.py
@@ -15,7 +15,7 @@ channelList = []
 
 def getChannels():
     print("Fetching Channel List...")
-    reqUrl = "https://jiotv.data.cdn.jio.com/apis/v1.3/getMobileChannelList/get/?os=android&devicetype=phone"
+    reqUrl = "https://jiotv.data.cdn.jio.com/apis/v1.4/getMobileChannelList/get/?os=android&devicetype=phone"
     try:
         response = requests.get(reqUrl, headers=headers, timeout=10)
         if response.status_code == 200:
